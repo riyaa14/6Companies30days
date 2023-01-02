9201021e6de2d63238a1bca0bee7db372609be78
Riya Garg <96983874+riyaa14@users.noreply.github.com>
2023-01-03 00:53:01 +0530

    Update 150. Evaluate Reverse Polish Notation.cpp

--- Microsoft/150. Evaluate Reverse Polish Notation.cpp
+++ Microsoft/150. Evaluate Reverse Polish Notation.cpp
@@ -1,1 +1,51 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens){
 
+        stack<int> s;
+        int val;
+        for(int i=0; i<tokens.size(); i++){
+
+            if(tokens[i] == "+" or tokens[i] == "-" or tokens[i] == "*" or tokens[i] == "/"){
+                string opt = tokens[i];
+                
+                    if(opt == "+"){ 
+                        int low = s.top();
+                        s.pop();
+                        int high = s.top();
+                        s.pop();
+                        s.push(high+low);
+                       
+                    }
+                    else if(opt == "*"){
+                        int low = s.top();
+                        s.pop();
+                        int high = s.top();
+                        s.pop();
+                        s.push(high*low);
+                        
+                    }
+                    else if(opt == "-"){
+                        int low = s.top();
+                        s.pop();
+                        int high = s.top();
+                        s.pop();
+                        s.push(high-low);
+                       
+                    }else if(opt == "/"){
+                        int low = s.top();
+                        s.pop();
+                        int high = s.top();
+                        s.pop();
+                        s.push(high/low);
+                        
+                    }
+               
+            }else{
+                s.push(stoi(tokens[i]));
+            }
+        }
+
+        return s.top();
+    }
+};
